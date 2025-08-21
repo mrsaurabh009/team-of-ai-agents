5b65cc56ea709efbcefe3d0eac21b7a49148e05e
Saurabh Kumar <saurabhkr1112004@gmail.com>
2025-08-21 13:15:30 +0530

    migrate dialogue_agent_with_tools.py to AgentExecutorShim (XAgentAdapter)

--- apps/server/agents/agent_simulations/agent/dialogue_agent_with_tools.py
+++ apps/server/agents/agent_simulations/agent/dialogue_agent_with_tools.py
@@ -1,15 +1,16 @@
 from typing import List, Optional
 
-from langchain.agents import AgentType, initialize_agent
 from langchain.schema import AIMessage, SystemMessage
 from langchain_community.chat_models import ChatOpenAI
 
 from agents.agent_simulations.agent.dialogue_agent import DialogueAgent
-from agents.conversational.output_parser import ConvoOutputParser
 from config import Config
 from memory.zep.zep_memory import ZepMemory
 from services.run_log import RunLogsManager
 from typings.agent import AgentWithConfigsOutput
+
+#  Use our new shim instead of LangChain ReAct agent
+from adapters.agent_executor_shim import AgentExecutorShim
 
 
 class DialogueAgentWithTools(DialogueAgent):
@@ -39,10 +40,7 @@ class DialogueAgentWithTools(DialogueAgent):
         and returns the message string
         """
 
-        memory: ZepMemory
-
-        # FIXME: This is a hack to get the memory working
-        # if self.is_memory:
+        # Setup memory
         memory = ZepMemory(
             session_id=self.session_id,
             url=Config.ZEP_API_URL,
@@ -54,38 +52,26 @@ class DialogueAgentWithTools(DialogueAgent):
         memory.human_name = self.sender_name
         memory.ai_name = self.agent_with_configs.agent.name
         memory.auto_save = False
-        # else:
-        #     memory = ConversationBufferMemory(
-        #         memory_key="chat_history", return_messages=True
-        #     )
 
         callbacks = []
-
         if self.run_logs_manager:
             self.model.callbacks = [self.run_logs_manager.get_agent_callback_handler()]
             callbacks.append(self.run_logs_manager.get_agent_callback_handler())
 
-        agent = initialize_agent(
-            self.tools,
-            self.model,
-            agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
-            verbose=True,
-            handle_parsing_errors=True,
-            memory=memory,
-            callbacks=callbacks,
-            agent_kwargs={
-                "system_message": self.system_message.content,
-                "output_parser": ConvoOutputParser(),
-            },
+        # Use XAgentAdapter through our shim
+        agent = AgentExecutorShim(
+            llm=self.model,
+            tools=self.tools,
+            prompt=self.system_message.content,
         )
 
+        # Build input from message history
         prompt = "\n".join(self.message_history + [self.prefix])
 
-        res = agent.run(input=prompt)
+        # Run agent
+        res = agent.run(input_text=prompt)
 
-        # FIXME: is memory
-        # memory.save_ai_message(res)
-
+        # Wrap result into LangChain AIMessage
         message = AIMessage(content=res)
 
         return message.content
